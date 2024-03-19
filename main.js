4715d42fc262ea131b6c59279b2f8863bbe51f61
Pedro nunes <111570874+Pedr0Nun3s@users.noreply.github.com>
2024-03-19 09:16:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const botoes = document.queryselectorall(".botao")
+
+for (let i = 0; i < botoes.length; i++){
+  botoes[i].onclick = function (){
+    for (let j = 0; j < botoes.length; j++){
+      botoes [j].classlist.remove("ativo");
+    }
+    botoes[i].classlist.add("ativo")
+  }
+}
 
